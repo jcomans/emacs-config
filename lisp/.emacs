702d4881203aee8e8571d90a709129f985098acc
Jan Comans <jan@jwork.comans.be>
2013-08-08 11:05:20 +0200

    Added new org mode for older Emacsen

--- lisp/.emacs
+++ lisp/.emacs
@@ -10,3 +10,4 @@
 (load "~/.emacs.d/lisp/latex-config"     nil t)
 (load "~/.emacs.d/lisp/yasnippet-config" nil t)
 (load "~/.emacs.d/lisp/magit-config"     nil t)
+(load "~/.emacs.d/lisp/org-mode-config"  nil t)
---
+++ lisp/org-mode-config.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org-mode configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/lisp/vendor/org")
+
+(setq org-directory "~/Dropbox/org-mode/desktop")
+
+(setq org-agenda-files (quote ("~/Dropbox/org-mode/desktop/refile.org")))
+
+(setq org-mobile-inbox-for-pull "~/Dropbox/org-mode/desktop/refile-mobile.org")
+(setq org-mobile-directory "~/Dropbox/org-mode/mobile")      
