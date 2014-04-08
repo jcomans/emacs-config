1160203f103249c014f6e9a30295f4ab7d314af3
Jan Comans <jan@jwork.comans.be>
2014-04-08 11:11:10 +0200

    Added indenting for visual line mode

--- lisp/general-config.el
+++ lisp/general-config.el
@@ -45,6 +45,16 @@
 ;; Show matching parentheses
 (show-paren-mode 1)
 
+;; Use indenting in visual line mode
+(add-to-list 'load-path "~/.emacs.d/lisp/vendor/adaptive-wrap")
+(require 'adaptive-wrap)
+
+(when (fboundp 'adaptive-wrap-prefix-mode)
+  (defun my-activate-adaptive-wrap-prefix-mode ()
+    "Toggle `visual-line-mode' and `adaptive-wrap-prefix-mode' simultaneously."
+    (adaptive-wrap-prefix-mode (if visual-line-mode 1 -1)))
+  (add-hook 'visual-line-mode-hook 'my-activate-adaptive-wrap-prefix-mode))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Behavior
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
